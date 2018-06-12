fe59bbc787f90e2ceb735ec89fef2c244d5a3dfe
Isuru <isuru71@gmail.com>
2018-06-12 23:53:32 +0530

    routing and sample components

---
+++ src/components/AppContainer.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Drugs from './drugs.component/drugs.component';
+import Patients from './patients.component/patients.component';
+
+export default class AppContainer extends Component {
+    render() {
+        return <div>
+            <h2>Pharmacy</h2>
+            <Router>
+                <div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Link to="/">Drugs</Link>
+                        </div>
+                        <div className="col-sm">
+                            <Link to="/about">Patients</Link>
+                        </div>
+                    </div>
+                    <Route exact path="/" render={props => {
+                        return <Drugs/>
+                    }}/>
+                    <Route path="/about" render={props => {
+                        return <Patients/>
+                    }}/>
+                </div>
+            </Router>
+        </div>;
+    }
+}
---
+++ src/components/drugs.component/drugs.component.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Drugs extends Component {
+    render() {
+        return <div>
+            Welcome to world of react
+        </div>;
+    }
+}
